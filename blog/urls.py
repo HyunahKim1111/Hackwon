df12adcf3141eebcccc2faa86fbf50f3341295e3
HyunahKim1111 <wingsoul11@naver.com>
2024-03-05 15:22:33 +0900

    블로그 기능 CRUD완료

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_list, name='blog_list'),
+    path('detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('upload/', BlogUploadView.as_view(), name='blog_upload'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,42 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Blog
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.detail import DetailView
+from django.shortcuts import redirect
 
 def blog_list(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/list.html', {'blogs':blogs})
+
+class BlogDetailView(DetailView):
+    model = Blog
+    fields = ['photo','text']
+    template_name = 'blog/detail.html'
+
+class BlogUploadView(CreateView):
+    model = Blog
+    fields = ['photo', 'text']
+    template_name = 'blog/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else: 
+            return self.render_to_response({'form':form})
+        
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = '/'
+    template_name = 'blog/delete.html'
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['photo','text']
+    template_name = 'blog/update.html'
+    
